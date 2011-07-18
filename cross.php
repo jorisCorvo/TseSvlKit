b1fae97355d4e18d456809d5717d6b017df3a949
CORVO Joris <joris.corvo@live.fr>
2011-07-18 14:52:10 +0200

    requete_ajax

---
+++ cross.php
@@ -0,0 +1,5 @@
+<?php
+// Change this to allow <yourdomain.com> to make it accessible to your site, or allow <*> for ANYONE to be able to access it.
+header('Access-Control: allow <http://127.0.0.1:8888>');
+?>
+<b>John Resig</b>
--- js/utils.js
+++ js/utils.js
@@ -1,15 +1,24 @@
-function simpleHttpRequest(url, success, failure) {
+function simpleHttpRequest(url) {
   var request = makeHttpObject();
   request.open("GET", url, true);
   request.send(null);
   request.onreadystatechange = function() {
-    if (request.readyState == 4) {
-      if (request.status == 200){
-	  
-        success(request.responseText);
-		return request.responseText;
-      else if (failure)
-        failure(request.status, request.statusText);
-    }
-  };
+    if (request.readyState == 4) 
+      if (request.status == 200)
+	   	return request.responseText;
+	};
 }
+
+function makeHttpObject() {
+  try {return new XMLHttpRequest();}
+  catch (error) {}
+  try {return new ActiveXObject("Msxml2.XMLHTTP");}
+  catch (error) {}
+  try {return new ActiveXObject("Microsoft.XMLHTTP");}
+  catch (error) {}
+
+  throw new Error("Could not create HTTP request object.");
+}
+function tester(){
+	return "crap";
+	}
